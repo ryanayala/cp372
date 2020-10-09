81a727e9129a6a367293f35565c910d63fd27678
Colin <33527481+ArticWhite@users.noreply.github.com>
2020-10-09 14:16:44 -0400

    Add files via upload

--- LibraryProtocol.java
+++ LibraryProtocol.java
@@ -1,24 +1,29 @@
-package protocolproject;
+package objectHolder;
 import java.net.*;
 import java.io.*;
 
 public class LibraryProtocol {
 	private static final int WAITFR = 0;      // wait for request.
-    private static final int SHUTTINGDOWN = 1;//prepares to shutdown.
-    
-    private String commands = "SUEDP" ;
+	private static final int INUSE = 1;
+    private static final int SHUTTINGDOWN = 2;//prepares to shutdown.
+    //SUEDP are the command keywords sent from client, requesting an action.
+    private String commands = "SUEDR" ;
     
     int state = WAITFR;
     
-    /* @method processes the given data
+    /*
+     *  @method processes the given data
      *  returns a array of the command, ISBN, author, title, year 
      */
     public String[] processInput(String theInput) {
-        String command[] = {"","","","",""};
+    	
+        String command[] = {"","","","","",""};
         String subString = theInput.toUpperCase();
-        
         if (state == WAITFR && theInput.length() > 0) {
-        	if ((theInput.charAt(0) !=',') && (commands.contains(Character.toString(theInput.charAt(0)).toUpperCase())) && (theInput.charAt(1) == ',') ) {
+        	if ((theInput.charAt(0) !=',') && (commands.contains(
+        			Character.toString(theInput.charAt(0)).toUpperCase())) &&
+        			(theInput.charAt(1) == ',') ) {
+        		
         		for (int i = 0; i < 5; i++) {
         			command[i] = subString.substring(0,subString.indexOf(',')-1);
         			subString = subString.substring(subString.indexOf(',')+1);
@@ -27,15 +32,15 @@ public class LibraryProtocol {
         }
         return command;
     	}
-    
-    /* @method validated the ISBN
+    /*
+     *  @method validated the ISBN
      * check to see if it length of 10 or 13 and year published.
      */
-    public boolean validateIsbn(String isbn, String year) {//object array is just the filler
+    public boolean validateIsbn(String isbn) {//object array is just the filler
+    	
     	try {  
-    	    int numYear = Integer.parseInt(year);
     	    Integer.parseInt(isbn);
-    	    if ((isbn.length() == 13 && numYear >= 2007) || ((isbn.length() == 10 && numYear <= 2007))) {
+    	    if ((isbn.length() == 13)) {
         		//requires algorithm to check isbn number
     	    	return true;
         	}
@@ -44,9 +49,8 @@ public class LibraryProtocol {
       	    }
     	return false;
         }
-    	
-    
     public boolean validateYear(String year) {
+    	
     	try {  
     	    int numYear = Integer.parseInt(year);
     	    if (numYear < 0 || numYear > 2020) { 
@@ -57,24 +61,72 @@ public class LibraryProtocol {
     	  }
     	return false;
     	}
-    
-    public String[] searchDatabase(Object Item, String[] parameters) {
-    	String result[] = null;
-    	//object method call.
-    	return result;
+    /* 
+	 * @
+	 */
+    public boolean insert(Database database, String[] data) {
+    	
+    	return database.insert(data);
+    }
+    /* 
+	 * @
+	 */
+    public String searchDatabase(Database database, String[] data) {
+    	
+    	return database.search(data);
+    }
+    /* 
+	 * @
+	 */
+    public boolean editDatabase(Database database, String[] data) {	
+    	
+    	return database.editDatabase(data);
+    }
+    /* 
+	 * @
+	 */
+    public boolean deteleFromDatabase(Database database, String[] data) {
+    	
+    	return database.delete(data);
+    }
+    /* 
+	 * @
+	 */
+    public String mostRecent(Database database) {
+    	
+    	return database.mostRecentlyAddedBook();
     }
     
-    public boolean editDatabase(Object item, String[] parameters) {
-    	boolean editMade = false;
-    	//call object method so search return object, and than edit object.
-    	return editMade;
-    }
-    
-}
-/*action = Character.toString(theInput.charAt(0));
-subString = theInput.substring(theInput.indexOf(',')+1);
-isbn = subString.substring(0,subString.indexOf(',')-1);
-String isbn = null;
-String subString = null;
+    public String protocolMethod(Database database, String input) {
+    	
+    	String responce = "No action taken";
+    	String[] data = processInput(input);
+    	if (validateIsbn(data[1]) == false) {
+    		return "Invalid ISBN";
+    	}
+    	//also possible add of isbn.
 
-*/
+    	if (state == WAITFR) {	
+    		switch (data[0]) {
+    		  case "S":
+    		    this.searchDatabase(database, data);
+    		    break;
+    		  case "U":
+    		    this.insert(database, data);
+    		    break;
+    		  case "E":
+    		    this.editDatabase(database, data);
+    		    break;
+    		  case "D":
+    		    System.out.println("Thursday");
+    		    break;
+    		  case "R":
+    			  
+    		}
+    	}
+    	
+    	return responce;
+    }
+    	
+}
+
